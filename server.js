0e73e6c368d462ec5bfb8c9f249d3d96a39c91f7
Sushma-abburi <sushma.a@dhatvibs.com>
2025-11-28 10:17:41 +0530

    server updated

--- server.js
+++ server.js
@@ -43,12 +43,18 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // For form-data support
+// app.use(cors({
+//   origin: [
+//     "https://employe-connect.dhatvibs.com",
+//     "https://internal-website-rho-vercel.app",
+//     "http://localhost:3000"
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true
+// }));
 app.use(cors({
-  origin: [
-    "https://employe-connect.dhatvibs.com",
-    "https://internal-website-rho-vercel.app",
-    "http://localhost:3000"
-  ],
+  origin: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
