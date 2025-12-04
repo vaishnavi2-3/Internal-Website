4fe018f9cbe7db0451e6d72dcf0b52f15aa0caa2
vaishnavi.k <vaishnavi.k@dhatvibs.com>
2025-12-04 15:05:29 +0530

    local host 5176 added

--- server.js
+++ server.js
@@ -61,6 +61,8 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+        "http://localhost:5176",
+
 
     "http://localhost:3000",
     "https://employe-connect.dhatvibs.com"
