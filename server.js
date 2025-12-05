4ab4596a3831815b2aa3b885de0c4d79487411d5
vaishnavi.k <vaishnavi.k@dhatvibs.com>
2025-12-05 11:12:11 +0530

    server updated

--- server.js
+++ server.js
@@ -46,29 +46,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // For form-data support
-// app.use(cors({
-//   origin: [
-//     "https://employe-connect.dhatvibs.com",
-//     "https://internal-website-rho-vercel.app",
-//     "http://localhost:3000"
-//   ],
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// }));
 app.use(cors({
-  origin: [
-    "*",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-    "http://localhost:5177",
-
-
-    "http://localhost:3000",
-    "https://employe-connect.dhatvibs.com"
-  ],
+  origin: (origin, callback) => {
+    callback(null, origin || "*"); // allow all origins
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
